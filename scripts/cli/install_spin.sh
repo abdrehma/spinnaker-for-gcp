c637ccd04b68a3111f6c3215b866d2b31b8d4863
Abdul Rehman <abdul.rehman@publicisgroupe.net>
2019-09-17 11:49:54 +0100

    Start hal deamon

--- scripts/cli/install_spin.sh
+++ scripts/cli/install_spin.sh
@@ -5,7 +5,7 @@ curl -LO https://storage.googleapis.com/spinnaker-artifacts/spin/$(curl -s https
 chmod +x spin
 mv spin ~
 
-grep -q '^alias spin=~/spin' ~/.bashrc || echo 'alias spin=~/spin' >> ~/.bashrc
+grep -q '^alias spin=~/spin' ~/.bashrc || echo 'alias spin=~/spin' >>~/.bashrc
 
 mkdir -p ~/.spin
 
@@ -22,8 +22,8 @@ fi
 source ~/spinnaker-for-gcp/scripts/install/properties
 
 # Query for static ip address as a signal that the Spinnaker installation is exposed via a secured endpoint.
-export IP_ADDR=$(gcloud compute addresses list --filter="name=$STATIC_IP_NAME" \
-  --format="value(address)" --global --project $PROJECT_ID)
+# export IP_ADDR=$(gcloud compute addresses list --filter="name=$STATIC_IP_NAME" \
+#   --format="value(address)" --global --project $PROJECT_ID)
 
 # Only re-generate ~/.spin/config if Spinnaker installation in unsecured. Otherwise, leave whatever is there.
 # The ~/.spin/config will always be restored by pull_config.sh in any case.
